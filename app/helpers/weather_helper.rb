64b232dad85a6211b4c7d25494ae1971b5834f5b
Jon Busby <jon.busby@wearetwogether.com>
2017-02-14 08:38:41 +0000

    fixing sun during the day

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -1,6 +1,6 @@
 module WeatherHelper
 
 	def weather_icon(constant)
-		return constant.downcase.gsub("_", "-").gsub("foggy","fog")
+		return constant.downcase.gsub("_", "-").gsub("foggy","fog").gsub("sun","day-sunny")
 	end
 end
