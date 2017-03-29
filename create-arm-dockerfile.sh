e29227d5f1e28d6f326cfc0589ce51bfbff7a1f5
Jon Busby <jon.busby@wearetwogether.com>
2017-03-29 19:41:54 +0100

    updating docker compose file and auto script

--- create-arm-dockerfile.sh
+++ create-arm-dockerfile.sh
@@ -7,3 +7,12 @@ sed -i '' 's/ruby/busbyjon\/armv6-ruby/g' Dockerfile.arm
 sed -i '' 's/# cross-compile-start/RUN [ "cross-build-start" ]/g' Dockerfile.arm
 sed -i '' 's/# cross-compile-end/RUN [ "cross-build-end" ]/g' Dockerfile.arm
 
+
+rm docker-compose.arm.yml
+cp docker-compose.yml docker-compose.arm.yml
+
+sed -i '' 's/redis:latest/hypriot\/rpi-redis:latest/g' docker-compose.arm.yml
+sed -i '' 's/build: ./image: busbyjon\/jarvis:arm/g' docker-compose.arm.yml
+sed -i '' 's/environment:/env_file: ..\/secrets.env/g' docker-compose.arm.yml
+sed -i '' '/- REDIS_URL=redis:\/\/jarvis_redis\//d' docker-compose.arm.yml
+sed -i '' '/- RAILS_ENV=development/d' docker-compose.arm.yml
