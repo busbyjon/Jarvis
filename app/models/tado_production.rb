03c8193e8abd278d47f62ad88092eae820d0f750
Jon Busby <jon.busby@wearetwogether.com>
2017-07-20 13:34:07 +0100

    fixing tado config

--- app/models/tado_production.rb
+++ app/models/tado_production.rb
@@ -1,7 +1,7 @@
 class TadoProduction < Tado
 
 	def initialize
-		@conn = Faraday.new(:url => 'https://my.tado.com') do |faraday|
+		@conn = Faraday.new(:url => 'https://auth.tado.com') do |faraday|
 		  faraday.request  :url_encoded             # form-encode POST params
 		  faraday.response :logger                  # log requests to STDOUT
 		  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
